7449e5b47aacf867718347d823e825e98c132f31
Michael Bumann <michael@railslove.com>
2012-10-23 10:08:14 +0200

    add mongodb dependeny

--- metadata.rb
+++ metadata.rb
@@ -5,3 +5,4 @@ description      "Installs/Configures railslove"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          "0.1.4"
 depends          "logrotate"
+depends          "mongodb"
