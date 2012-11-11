0b7005bb40d1711f197b057e9e959a9e3f55732c
Michael Bumann <michael@railslove.com>
2012-11-11 18:02:56 +0100

    default rails home to /srv/www/

--- libraries/default.rb
+++ libraries/default.rb
@@ -4,7 +4,7 @@ module RailsloveSharedAttributes
     klass.attribute :cookbook, :kind_of => String, :default => "railslove"
     klass.attribute :user, :kind_of => String, :default => "rails"
     klass.attribute :group, :kind_of => String, :default => "rails"
-    klass.attribute :home, :kind_of => String, :default => "/srv/www"
+    klass.attribute :home, :kind_of => String, :default => "/srv/www/"
     klass.attribute :deploy_group, :kind_of => String, :default => "deployer"
   end
 end
