e9398c84954615bf72f6c91ac78c0dd6b5225ab0
Lars Brillert <lars@railslove.com>
2013-01-14 10:01:52 +0100

    creates and updates public hostnames on inwx

--- recipes/hostname.rb
+++ recipes/hostname.rb
@@ -17,5 +17,64 @@
 # limitations under the License.
 #
 
+# class Chef::Recipe
+#   include INWX::Domrobot
+# end
+
+#::Chef::Recipe.send(:include, Opscode::Mysql::Helpers)
+
 node.set[:set_fqdn] = node.name + "." + node.chef_environment + ".#{node[:railslove][:domain]}"
 include_recipe "hostname"
+
+#load "inwx/Domrobot.rb"
+require "yaml"
+
+#addr = "api.ote.domrobot.com"
+addr = "api.domrobot.com"
+
+credentials = data_bag_item("inwx", "credentials")
+
+user = credentials["login"]
+pass = credentials["password"]
+
+domrobot = INWX::Domrobot.new(addr)
+
+result = domrobot.login(user,pass)
+
+content = node.ec2.public_ipv4
+name    = "#{node.name}.#{node.chef_environment}"
+domain  = node.railslove.domain
+
+object = "nameserver"
+method = "info"
+
+params = { :name => name, :domain => domain }
+
+result = domrobot.call(object, method, params)
+
+puts YAML::dump(result)
+if result["code"] == 1000
+  record  = result["resData"]["record"].first
+
+  object = "nameserver"
+  method = "updateRecord"
+
+  params = { :id => record["id"], :type => "A", :content => content, :name => name }
+
+  if record["content"] == content
+    puts "not updating..."
+  else
+    result = domrobot.call(object, method, params)
+
+    puts YAML::dump(result)
+  end
+else
+  object = "nameserver"
+  method = "createRecord"
+
+  params    = { :type => "A", :content => content, :name => name, :domain => domain }
+
+  result = domrobot.call(object, method, params)
+
+  puts YAML::dump(result)
+end
