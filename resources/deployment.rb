369852c2a45feb0119f020d030fb01f69a0113da
Michael Bumann <michael@railslove.com>
2013-02-01 15:28:32 +0100

    by default do not die if rake db:crate or rake db:migrate fails on deployment

--- resources/deployment.rb
+++ resources/deployment.rb
@@ -12,7 +12,7 @@ include RailsloveSharedAttributes
 attribute :site_config, :kind_of => Hash, :required => true
 # these are default values, which should actually be definded in the application databag
 attribute :migrate, :kind_of => [TrueClass, FalseClass, NilClass], :default => true
-attribute :migration_command, :kind_of => String, :default => "bundle exec rake db:create && bundle exec rake db:migrate" # we make sure that the db exists before running the migration
+attribute :migration_command, :kind_of => String, :default => 'bundle exec rake db:create db:migrate --trace; echo "rake db:create db:migrate exited with $?"'# we make sure that the db exists before running the migration
 attribute :restart_command, :kind_of => String, :default => "touch tmp/restart.txt"
 attribute :precompile_assets, :kind_of => [NilClass, TrueClass, FalseClass], :default => true
 attribute :revision, :kind_of => String, :default => "master"
