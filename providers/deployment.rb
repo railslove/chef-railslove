aefa2546d094a79a9f484d0c6c46e4fa2c5bba34
Michael Bumann <michael@railslove.com>
2013-02-21 12:34:41 +0100

    Revert "set locale env vars for deployment."
    This reverts commit b1afa61b626f7744c2a517309410c675611264d1.

--- providers/deployment.rb
+++ providers/deployment.rb
@@ -55,11 +55,8 @@ action :deploy do
     environment_name deploy_config[:environment]
     environment({
       "RAILS_ENV" => deploy_config[:environment],
-      "RACK_ENV"  => deploy_config[:environment],
-      'LC_ALL' => 'en_GB.UTF-8',
-      'LANG'   => 'en_GB.UTF-8'
+      "RACK_ENV"  => deploy_config[:environment]
     })
-
     repository deploy_config[:repository]
     revision deploy_config[:revision]
 
