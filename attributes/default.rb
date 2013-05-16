40ac0b671a28903ed6f6f41c90f16ba10b9fa91f
Lars Brillert <lars@railslove.com>
2013-05-16 23:21:40 +0200

    change mange dns record	by default

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,6 +1,6 @@
 default['railslove']['packages'] =  [] # specifiy addons eg mysql, postgresql or ldap
 default['railslove']['companies'] = ["railslove"] # specifiy which companies should have access
 default['railslove']['domain'] = "railslabs.com"
-default['railslove']['manage_dns_records'] = false
+default['railslove']['manage_dns_records'] = true
 default['railslove']['route53']['databag'] = "aws"
 default['railslove']['route53']['item'] = "route53"
