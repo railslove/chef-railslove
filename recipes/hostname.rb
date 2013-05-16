c6a328cecf0718f381097e84c897e7fa53fab1f9
Lars Brillert <lars@railslove.com>
2013-05-16 23:10:56 +0200

    use `cloud` over `ec2` and be more resilient against missing data bags

--- recipes/hostname.rb
+++ recipes/hostname.rb
@@ -17,25 +17,31 @@
 # limitations under the License.
 #
 
+def route53_bag
+  data_bag_item(node['railslove']['route53']['databag'], node['railslove']['route53']['item'])
+rescue
+  Chef::Log.warn("You need to provide access credentials for Amazon Route53")
+  {}
+end
+
 node.set['set_fqdn'] = node.name + "." + node.chef_environment + ".#{node['railslove']['domain']}"
 include_recipe "hostname"
 
-if node['railslove']['manage_dns_records']
-  gem_package "fog"
-  credentials = data_bag_item(node['railslove']['route53']['databag'], node['railslove']['route53']['item'])
+gem_package "fog"
+cloud       = node['cloud'] || {}
+credentials = route53_bag
 
-  route53_record "create a record" do
-    name  node['set_fqdn']
-    value node['ec2']["public_ipv4"]
-    type  "A"
-    ttl 300
+route53_record "create a record" do
+  name node['set_fqdn']
+  value cloud["public_ipv4"]
+  type  "A"
+  ttl 300
 
-    zone_id               credentials["zone_id"]
-    aws_access_key_id     credentials["aws_access_key_id"]
-    aws_secret_access_key credentials["aws_secret_access_key"]
+  zone_id               credentials["zone_id"]
+  aws_access_key_id     credentials["aws_access_key_id"]
+  aws_secret_access_key credentials["aws_secret_access_key"]
 
-    action :create
+  action :create
 
-    only_if { node.attribute?('ec2') and node['ec2'].attribute?('public_ipv4') }
-  end
+  only_if { node['railslove']['manage_dns_records'] and credentials.any? }
 end
