f759d123b1b1f2b8a45b6f0cabf43a48551c016b
Lars Brillert <lars@railslove.com>
2014-07-04 15:35:09 +0200

    no need to have this frozen to an old version, we're using chef 11 everywhere

--- metadata.rb
+++ metadata.rb
@@ -4,11 +4,11 @@ maintainer_email "lars@railslove.com"
 license          "Apache 2.0"
 description      "Installs/Configures railslove"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          "0.8.0"
+version          "0.8.1"
 
 depends          "logrotate"
 depends          "mongodb"
-depends          "application", "2.0.4"
+depends          "application"
 depends          "users"
 depends          "hostname"
 depends          "route53"
