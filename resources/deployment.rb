6eb7e658f38411e3ec0bd59f8a2c1da9535a4772
Lars Brillert <lars@railslove.com>
2015-01-19 17:41:22 +0100

    needs to be specified here

--- resources/deployment.rb
+++ resources/deployment.rb
@@ -12,6 +12,7 @@ include RailsloveSharedAttributes
 
 attribute :site_config, :kind_of => Hash, :required => true
 # these are default values, which should actually be definded in the application databag
+attribute :bundler, :kind_of => [NilClass, TrueClass, FalseClass], :default => true
 attribute :migrate, :kind_of => [TrueClass, FalseClass, NilClass], :default => true
 attribute :migration_command, :kind_of => String, :default => 'bundle exec rake db:create db:migrate --trace; echo "rake db:create db:migrate exited with $?"'# we make sure that the db exists before running the migration
 attribute :restart_command, :kind_of => String, :default => "touch tmp/restart.txt"
