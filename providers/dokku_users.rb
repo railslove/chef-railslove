722e88166ef7b790c0a54defcaca737583724748
Lars Brillert <lars@railslove.com>
2018-01-05 13:17:59 +0100

    add/remove of dokku users

---
+++ providers/dokku_users.rb
@@ -0,0 +1,29 @@
+#
+# Cookbook Name:: dokku
+# Provider:: ssh_key
+#
+# Copyright (c) 2015 Nick Charlton, MIT licensed.
+#
+
+def whyrun_supported?
+  true
+end
+
+action :add do
+  execute "adding ssh key for #{new_resource.user}" do
+    command "sshcommand acl-remove dokku #{new_resource.user}"
+    command "echo #{new_resource.key} | sshcommand " \
+            "acl-add dokku #{new_resource.user}"
+
+    not_if do
+      system("grep #{new_resource.key} /home/dokku/.ssh/authorized_keys")
+    end
+  end
+end
+
+action :remove do
+  execute "removing ssh key for #{new_resource.user}" do
+    command "echo #{new_resource.key} | sshcommand " \
+            "acl-remove dokku #{new_resource.user}"
+  end
+end
---
+++ recipes/dokku.rb
@@ -0,0 +1,29 @@
+#
+# Cookbook Name:: railslove
+# Recipe:: dokku
+#
+# Copyright 2017, Railslove GmbH
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+if Chef::Config[:solo]
+  Chef::Log.warn("This recipe uses search. Chef Solo does not support search.")
+end
+
+search(:users, "(#{node['roles'].map{|r| "groups:#{r}" }.join(" OR ")})").each do |user|
+  dokku_user do
+    key user['ssh_keys']
+    user user['id']
+    action user.fetch('action', 'add')
+  end
+end
