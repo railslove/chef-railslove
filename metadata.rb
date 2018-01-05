ae72f5557e73b0fadd574be495021418ff77a7a9
Lars Brillert <lars@railslove.com>
2018-01-05 14:04:52 +0100

    we can do it

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email "lars@railslove.com"
 license          "Apache 2.0"
 description      "Installs/Configures railslove"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          "1.4.6"
+version          "1.4.7"
 
 depends          "logrotate"
 depends          "mongodb"
--- providers/dokku_user.rb
+++ providers/dokku_user.rb
@@ -12,10 +12,8 @@ end
 use_inline_resources
 action :add do
   execute "adding ssh key for #{new_resource.user}" do
-    command "sshcommand acl-remove dokku #{new_resource.user}"
-    command "echo #{new_resource.key} | sshcommand acl-add dokku #{new_resource.user}" do
-      not_if { system("grep #{new_resource.key} /home/dokku/.ssh/authorized_keys") }
-    end
+    command "sshcommand acl-remove dokku #{new_resource.user} && echo #{new_resource.key} | sshcommand acl-add dokku #{new_resource.user}"
+    not_if { system("grep #{new_resource.key} /home/dokku/.ssh/authorized_keys") }
   end
   new_resource.updated_by_last_action(true)
 end
